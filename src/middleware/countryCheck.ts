ed0b6f30bd9a9fbc3aa4ef48bea0b94e6f8cc49d
HemantOcode <palh8684@gmail.com>
2025-12-09 05:52:20 +0530

    deployed

---
+++ src/middleware/countryCheck.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import UserService from "../users/user.service";
+import { AllowedCountry } from "../Helper/CountryList";
+import { getIPFromReq } from "../Helper/getIP";
+
+const userService = new UserService();
+
+export const CountryCheck = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    let ip = getIPFromReq(req);
+    let country = await userService.getUserCountry(ip ?? "");
+    console.log(country);
+    if (AllowedCountry.includes(country)) {
+      return next();
+    } else {
+      return res
+        .status(401)
+        .json({ success: false, message: "Not allowed on your region" });
+    }
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Something went wrong" });
+  }
+};
